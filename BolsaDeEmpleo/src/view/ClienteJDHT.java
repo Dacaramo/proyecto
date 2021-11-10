81ace3012b48ba80557e7fcf33850c3a13156c66
Dacaramo <dankramirez@outlook.com>
2021-11-10 01:45:15 -0500

    JDHT Tests completed

--- BolsaDeEmpleo/src/view/ClienteJDHT.java
+++ BolsaDeEmpleo/src/view/ClienteJDHT.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.io.IOException;
+import java.util.Scanner;
 import org.kth.dks.JDHT;
 import org.kth.dks.dks_exceptions.DKSIdentifierAlreadyTaken;
 import org.kth.dks.dks_exceptions.DKSRefNoResponse;
@@ -8,14 +9,34 @@ import org.kth.dks.dks_exceptions.DKSTooManyRestartJoins;
 
 public class ClienteJDHT {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        char salir = 'n';
+        String ref, llave;
+        Scanner sc = new Scanner(System.in);
 
         try 
         {
-            JDHT myDHT = new JDHT(5550, "dksref://192.168.1.10:4440/0/4220662422/0/6885128012772432190");
-
-            String value = (String) myDHT.get("Java SE API");
-            System.out.println("\n\n" + value);
+            System.out.print("\n\nIngrese el ref de la DHT generado por el otro programa: ");
+            ref = sc.next();  
+            
+            JDHT myDHT = new JDHT(5550, ref);
+            
+            do
+            {
+                System.out.print("\n");
+                
+                System.out.print("Ingrese la llave del valor a buscar en la DHT: ");
+                llave = sc.next();
+                
+                String value = (String) myDHT.get(llave);
+                System.out.print("El valor es " + value);
+                
+                System.out.print("\n\n");
+                
+                System.out.print("Desea salir del programa? (s: si/n: no): ");
+                salir = sc.next().charAt(0);
+            }
+            while(salir != 's');
+            
             myDHT.close();
         }
         catch (IOException | DKSTooManyRestartJoins |
--- BolsaDeEmpleo/src/view/ProveedorJDHT.java
+++ BolsaDeEmpleo/src/view/ProveedorJDHT.java
@@ -2,17 +2,39 @@ package view;
 
 import java.io.IOException;
 import java.util.Scanner;
+import model.ConsoleColors;
 import org.kth.dks.JDHT;
 
 public class ProveedorJDHT {
     public static void main(String[] args) throws Exception {
+        char salir = 'n';
+        String llave, valor;
+        Scanner sc = new Scanner(System.in);
+        
         try 
         {
             JDHT DHTExample = new JDHT();
-            DHTExample.put("Java SE API", "http://docs.oracle.com/javase/8/docs/api/");
-
-            System.out.println("\n\n" + ((JDHT) DHTExample).getReference());
-
+            System.out.println("\n" + ConsoleColors.GREEN_BACKGROUND + ((JDHT) DHTExample).getReference() + ConsoleColors.RESET);
+            
+            do
+            {
+                System.out.print("\n");
+                
+                System.out.print("Ingrese la llave de la nueva entrada en la DHT: ");
+                llave = sc.next();
+                
+                System.out.print("Ingrese el valor de la nueva entrada en la DHT: ");
+                valor = sc.next();
+                
+                System.out.print("\n");
+                
+                DHTExample.put(llave, valor);
+                
+                System.out.print("Desea salir del programa? (s: si/n: no): ");
+                salir = sc.next().charAt(0);
+            }
+            while(salir != 's');
+            
             Scanner scanner = new Scanner(System.in);
             System.out.println("Press Enter to terminate application... ");
             scanner.next();
